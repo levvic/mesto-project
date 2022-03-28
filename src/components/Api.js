ffd143ffbaf16d6ae70efa7cc9f3edb02ec705d9
Anton.Aduisky <a.mltw@yandex.ru>
2022-03-28 18:32:45 +0300

    feat:api
    Co-authored-by: Victoria Panchenko <VictoriaPanchenko@users.noreply.github.com>

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,26 +1,28 @@
 export default class Api {
-  constructor() {
-    this._token = "bc5524e6-2f6e-4891-adc9-e477685018b2";
-    this._config = {
+  constructor(options) {
+    this._headers = options.headers;
+    this._baseUrl = options.baseUrl;
+
+    /*this._config = {
       baseUrl: "https://nomoreparties.co/v1/plus-cohort7",
       headers: {
-        authorization: this._token,
+        authorization: "bc5524e6-2f6e-4891-adc9-e477685018b2",
         "Content-Type": "application/json",
       }
-    };
+    };*/
   }
 
   getUserInfo() {
-    return fetch(`${this._config.baseUrl}/users/me`, {
-        headers: this._config.headers,
+    return fetch(`${this._baseUrl}/users/me`, {
+        headers: this._headers,
       })
       .then(this._checkResponse);
   };
 
   patchProfileInfo(name, description) {
-    return fetch(`${this._config.baseUrl}/users/me`, {
+    return fetch(`${this._baseUrl}/users/me`, {
         method: "PATCH",
-        headers: this._config.headers,
+        headers: this._headers,
         body: JSON.stringify({
           name: name,
           about: description,
@@ -30,9 +32,9 @@ export default class Api {
   };
 
   patchAvatar(link) {
-    return fetch(`${this._config.baseUrl}/users/me/avatar`, {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
         method: "PATCH",
-        headers: this._config.headers,
+        headers: this._headers,
         body: JSON.stringify({
           avatar: link,
         }),
@@ -41,16 +43,16 @@ export default class Api {
   };
 
   getCards() {
-    return fetch(`${this._config.baseUrl}/cards`, {
-        headers: this._config.headers,
+    return fetch(`${this._baseUrl}/cards`, {
+        headers: this._headers,
       })
       .then(this._checkResponse);
   };
 
   postCard(name, link) {
-    return fetch(`${this._config.baseUrl}/cards`, {
+    return fetch(`${this._baseUrl}/cards`, {
         method: "POST",
-        headers: this._config.headers,
+        headers: this._headers,
         body: JSON.stringify({
           name: name,
           link: link,
@@ -60,25 +62,25 @@ export default class Api {
   };
 
   deleteCard(cardId) {
-    return fetch(`${this._config.baseUrl}/cards/${cardId}`, {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
         method: "DELETE",
-        headers: this._config.headers,
+        headers: this._headers,
       })
       .then(this._checkResponse);
   };
 
   putLike(cardId) {
-    return fetch(`${this._config.baseUrl}/cards/likes/${cardId}`, {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
         method: "PUT",
-        headers: this._config.headers,
+        headers: this._headers,
       })
       .then(this._checkResponse);
   };
 
   deleteLike(cardId) {
-    return fetch(`${this._config.baseUrl}/cards/likes/${cardId}`, {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
         method: "DELETE",
-        headers: this._config.headers,
+        headers: this._headers,
       })
       .then(this._checkResponse);
   };
