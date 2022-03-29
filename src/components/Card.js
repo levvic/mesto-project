b80828909648862e259a8cf9b5de7920eb458735
Anton.Aduisky <a.mltw@yandex.ru>
2022-03-29 23:25:55 +0300

    feat: card class
    Co-authored-by: Victoria Panchenko <VictoriaPanchenko@users.noreply.github.com>

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,153 +1,99 @@
-import FormValidator from "./FormValidator.js";
-
 import {
-  cardTemplate,
-  popupAddCard,
-  addCardBtn,
-  cardsList,
-  cardNameInput,
-  cardLinkInput,
-  newCardSubmitButton,
-  formAddCard,
-  buttonDisabledClass,
-  likeBtnActiveClass,
-  newCardSelector,
-  newCardImgSelector,
-  newCardNameSelector,
   likeBtnSelector,
   deleteBtnSelector,
-  numberOfLikes,
-} from "../utils/constants.js";
+  numberOfLikesSelector,
+  cardImgSelector,
+  likeBtnActiveClass,
+  newCardNameSelector
+} from "./utils/constants.js";
 
 export default class Card {
   constructor({
-    putLike,
-    deleteLike,
-    postCard,
-    deleteCard,
-    openPopup,
-    closePopup,
-    openPicContainer
-  }) {
-    this._putLike = putLike;
-    this._deleteLike = deleteLike;
-    this._postCard = postCard;
-    this._deleteCard = deleteCard;
+    id,
+    name,
+    link,
+    likeNmbr,
+    likedByMe,
+    createdByMe,
+    handleCardClick,
+    handleDeleteCard,
+    handleLikeCard
+  }, templateSelector) {
+    this._id = id;
+    this._name = name;
+    this._link = link;
+    this._likeNmbr = likeNmbr;
+    this._likedByMe = likedByMe;
+    this._createdByMe = createdByMe;
 
-    /*this._openPopup = openPopup;
-    this._closePopup = closePopup;
-    this._openPicContainer = openPicContainer;*/
-
-    this._validator = new FormValidator({
-      buttonDisabledClass
-    });
+    this._cardTemplateElement = document.querySelector(templateSelector);
+    this._handleCardClick = handleCardClick;
+    this._handleDeleteCard = handleDeleteCard;
+    this._handleLikeCard = handleLikeCard;
   }
 
-  createCard(
-    cardName,
-    cardLink,
-    cardId,
-    likedByMe = false,
-    likeNmbr = 0,
-    createdByMe = true
-  ) {
-    const newCard = cardTemplate.querySelector(newCardSelector).cloneNode(true);
-    const cardImg = newCard.querySelector(newCardImgSelector);
-    const cardTitle = newCard.querySelector(newCardNameSelector);
+  _getTemplate() {
+    const cardElement = this._cardTemplateElement.content.cloneNode(true);
+    return cardElement;
+  }
 
-    newCard.setAttribute("id", cardId);
-    cardImg.src = cardLink;
-    cardImg.alt = cardName;
-    cardTitle.textContent = cardName;
+  _setEventListeners(element) {
+    const deleteBtn = element.querySelector(deleteBtnSelector);
+    this._likeBtn = element.querySelector(likeBtnSelector);
+    const img = element.querySelector(cardImgSelector);
 
-    const likes = newCard.querySelector(numberOfLikes);
-    likes.textContent = likeNmbr;
-    const likeBtn = newCard.querySelector(likeBtnSelector);
-
-    if (likedByMe) {
-      likeBtn.classList.add(likeBtnActiveClass);
-    }
-
-    likeBtn.addEventListener("click", () => {
-      if (likeBtn.classList.contains(likeBtnActiveClass)) {
-        this._deleteLike()
-          .then((card) => {
-            this.updateNumberOfLikes(card._id, card.likes.length);
-            likeBtn.classList.toggle(likeBtnActiveClass);
-          })
-          .catch((err) => {
-            alert("Ошибка");
-            console.log(err);
-          });
-      } else {
-        this._putLike()
-          .then((card) => {
-            this.updateNumberOfLikes(card._id, card.likes.length);
-            likeBtn.classList.toggle(likeBtnActiveClass);
-          })
-          .catch((err) => {
-            alert("Ошибка");
-            console.log(err);
-          });
-      }
+    deleteBtn.addEventListener('click', () => {
+      this._handleDeleteCard(element, this._id)
     });
 
+    this._likeBtn.addEventListener('click', () => {
+      this._handleLikeCard(this._id);
+    });
+
+    img.addEventListener('click', (evt) => {
+      this._handleCardClick(evt);
+    });
+
+    //this._deleteBtn = element.querySelector(deleteBtnSelector);
+    //this._
+  }
+
+  _hideDeleteBtn(newCard) {
     const deleteBtn = newCard.querySelector(deleteBtnSelector);
     // user can delete only his own cards
-    if (createdByMe) {
-      deleteBtn.addEventListener("click", this.removeCard);
-    } else {
+    if (!this._createdByMe) {
       deleteBtn.style.display = "none";
     }
+  }
 
-    //cardImg.addEventListener("click", this._openPicContainer);
-    return newCard;
+  updateLikeCounter(isCardLiked, likeNumber) {
+    this._likedByMe = isCardLiked;
+    this._likeNmbr = likeNumber;
+
+    const likeNmbrElement = this._newCardElement.querySelector(numberOfLikesSelector);
+    likeNmbrElement.textContent = likeNumber;
+
+    if (this._likedByMe) {
+      this._likeBtn.classList.add(likeBtnActiveClass);
+    } else {
+      this._likeBtn.classList.remove(likeBtnActiveClass);
+    }
+  }
+
+  createCard() {
+    this._newCardElement = this._getTemplate();
+    this._setEventListeners(this._newCardElement);
+    this._hideDeleteBtn(this._newCardElement);
+    this.updateLikeCounter(this._likedByMe, this._likeNmbr);
+
+    const imgElement = this._newCardElement.querySelector(cardImgSelector);
+    const titleElement = this._newCardElement.querySelector(newCardNameSelector);
+
+    this._newCardElement.setAttribute("id", this._id);
+    imgElement.src = this._link;
+    imgElement.alt = this._name;
+    titleElement.textContent = this._name;
+
+    return this._newCardElement;
   };
-
-  updateNumberOfLikes(cardId, likesCount) {
-    const likes = document.getElementById(cardId).querySelector(numberOfLikes);
-    likes.textContent = likesCount;
-  };
-
-  removeCard(evt) {
-    const card = evt.srcElement.closest(".location-card");
-    this._deleteCard()
-      .then((res) => {
-        if (res) {
-          card.remove();
-        }
-      })
-      .catch((err) => {
-        alert("Ошибка");
-        console.log(err);
-      });
-  };
-
-  /*submitCardInfo(evt) {
-    evt.preventDefault();
-    newCardSubmitButton.textContent = "Создание...";
-    this._postCard()
-      .then((res) => {
-        cardsList.prepend(createCard(res.name, res.link, res._id));
-        cardNameInput.value = "";
-        cardLinkInput.value = "";
-        this._closePopup();
-        this._validator.disableButton(evt.submitter);
-      })
-      .catch((err) => {
-        alert("Ошибка");
-        console.log(err);
-      })
-      .finally(() => {
-        newCardSubmitButton.textContent = "Создать";
-      });
-  }; */
-
-  /*openCardPopup() {
-    this._openPopup()
-  }; */
-
-  //formAddCard.addEventListener("submit", submitCardInfo);
-  //  addCardBtn.addEventListener("click", openCardPopup);
-
 }
