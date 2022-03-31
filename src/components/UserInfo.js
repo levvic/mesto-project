0c9044fb47528c6fd366841189314441d3ad34fd
Victoria Panchenko <vkpanchenko@gmail.com>
2022-03-31 17:51:33 +0200

    feat: userinfo class
    Co-authored-by: AntonAduisky <AntonAduisky@users.noreply.github.com>

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,42 +1,24 @@
 export default class UserInfo {
-  //Принимает в конструктор объект с селекторами двух элементов: элемента имени пользователя и элемента информации о себе.
   constructor({
-    userName,
-    userDescription
-  }, getUserInformation, patchProfileInfo) {
-    this._userName = userName;
-    this._userDescription = userDescription;
-    this._getUserInformation = getUserInformation;
-    this._patchProfileInfo = patchProfileInfo;
+    userNameSelector,
+    userDescriptionSelector
+  }) {
+    this._userNameElement = document.querySelector(userNameSelector);
+    this._userDescriptionElement = document.querySelector(userDescriptionSelector);
   }
-  //публичный метод getUserInfo, который возвращает объект с данными пользователя.
+
   getUserInfo() {
 
-    this._getUserInformation()
-      .then(userData => userdata)
-      .catch((err) => {
-        alert("Ошибка");
-        console.log(err);
-      });
+    const user = {
+      name: this._userNameElement.textContent,
+      about: this._userDescriptionElement.textContent
+    }
 
-    /*return {
-      name: this._userName.textContent,
-      description: this._userDescription.textContent,
-    }*/
+    return user;
   }
-  //Содержит публичный метод setUserInfo, 
-  //который принимает новые данные пользователя
 
-  
-  setUserInfo() {
-    // отправляет их на сервер 
-    this._patchProfileInfo(this._userName, this._userDescription);
-
-    //render on client
-
-
-    /*this._userName.textContent = item.name;
-    this._userDescription.textContent = item.about;    */
-
+  setUserInfo(name, about) {
+    this._userNameElement.textContent = name;
+    this._userDescriptionElement.textContent = about;
   }
 }
